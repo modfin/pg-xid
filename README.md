0c5cf8445202d8434c5367fec7202c90b642b7d2
raz <rasmus.holm@modularfinance.se>
2022-04-06 18:00:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,162 @@
+# Globally Unique ID Generator
+
+[![license](http://img.shields.io/badge/license-MIT-red.svg?style=flat)](https://raw.githubusercontent.com/modfin/pg-xid/master/LICENSE)
+
+
+###### This project is a Postgres implementation of the Go Lang library found here: [https://github.com/rs/xid](https://github.com/rs/xid)
+
+---
+
+## Description
+
+`Xid` is a globally unique id generator library. They are small, fast to generate and ordered.
+
+Xid uses the *Mongo Object ID* algorithm to generate globally unique ids with a different serialization (base32) to make it shorter when transported as a string:
+https://docs.mongodb.org/manual/reference/object-id/
+
+<table border="1">
+<caption>Xid layout</caption>
+<tr>
+<td>0</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td>
+</tr>
+<tr>
+<td colspan="4">time</td><td colspan="3">machine id</td><td colspan="2">pid</td><td colspan="3">counter</td>
+</tr>
+</table>
+
+- 4-byte value representing the seconds since the Unix epoch,
+- 3-byte machine identifier,
+- 2-byte process id, and
+- 3-byte counter, starting with a random value.
+
+The binary representation of the id is compatible with Mongo 12 bytes Object IDs.
+The string representation is using [base32 hex (w/o padding)](https://tools.ietf.org/html/rfc4648#page-10) for better space efficiency when stored in that form (20 bytes). The hex variant of base32 is used to retain the
+sortable property of the id.
+
+`Xid`s simply offer uniqueness and speed, but they are not cryptographically secure. They are predictable and can be *brute forced* given enough time.
+
+## Features
+- Size: 12 bytes (96 bits), smaller than UUID, larger than [Twitter Snowflake](https://blog.twitter.com/2010/announcing-snowflake)
+- Base32 hex encoded by default (20 chars when transported as printable string, still sortable)
+- Configuration free: there is no need to set a unique machine and/or data center id
+- K-ordered
+- Embedded time with 1 second precision
+- Unicity guaranteed for 16,777,216 (24 bits) unique ids per second and per host/process
+- Lock-free (unlike UUIDv1 and v2)
+
+## Comparison
+
+| Name        | Binary Size | String Size    | Features
+|-------------|-------------|----------------|----------------
+| [UUID]      | 16 bytes    | 36 chars       | configuration free, not sortable
+| [shortuuid] | 16 bytes    | 22 chars       | configuration free, not sortable
+| [Snowflake] | 8 bytes     | up to 20 chars | needs machine/DC configuration, needs central server, sortable
+| [MongoID]   | 12 bytes    | 24 chars       | configuration free, sortable
+| xid         | 12 bytes    | 20 chars       | configuration free, sortable
+
+[UUID]: https://en.wikipedia.org/wiki/Universally_unique_identifier
+[shortuuid]: https://github.com/stochastic-technologies/shortuuid
+[Snowflake]: https://blog.twitter.com/2010/announcing-snowflake
+[MongoID]: https://docs.mongodb.org/manual/reference/object-id/
+
+## Installation
+
+```bash
+cat ./xid.sql | psql your-database
+```
+
+## Usage
+Generate a `xid` as base32Hex
+```sql
+SELECT xid() ;
+
+-- +--------------------+
+-- |xid                 |
+-- +--------------------+
+-- |c96r3a88u0k0b3e6hft0|
+-- +--------------------+
+```
+
+Generate a `xid` as a base32Hex at a specific time
+```sql
+SELECT xid, xid_time(xid) FROM(
+  SELECT xid( _at => CURRENT_TIMESTAMP - INTERVAL '10 YEAR')
+) a
+
+-- +--------------------+---------------------------------+
+-- |xid                 |xid_time                         |
+-- +--------------------+---------------------------------+
+-- |9tvgr208u0k0b3e6hgb0|2012-04-06 15:36:40.000000 +00:00|
+-- +--------------------+---------------------------------+
+
+```
+
+
+Use a xid as column type
+
+```sql
+
+CREATE TABLE users (
+    id public.xid PRIMARY KEY DEFAULT xid(),
+    email text
+);
+
+INSERT INTO users (email)
+VALUES
+('user1@example.com'),
+('user2@example.com');
+
+SELECT * FROM users;
+
+-- +--------------------+-----------------+
+-- |id                  |email            |
+-- +--------------------+-----------------+
+-- |c96r9eo8u0k0b3e6hgcg|user1@example.com|
+-- |c96r9eo8u0k0b3e6hgd0|user2@example.com|
+-- +--------------------+-----------------+
+
+
+```
+
+Decode a `xid` as byte array 
+```sql
+SELECT xid_decode(xid())
+-- +---------------------------------------+
+-- |xid_decode                             |
+-- +---------------------------------------+
+-- |{98,77,178,53,8,240,40,5,141,198,140,2}|
+-- +---------------------------------------+
+
+```
+
+Encode byte array as `xid`
+```sql
+SELECT xid_encode('{98,77,178,53,8,240,40,5,141,198,140,2}'::INT[]);
+-- +---------------------+
+-- |xid_encode           |
+-- +---------------------+
+-- |c96r4d88u0k0b3e6hg10 |
+-- +---------------------+
+
+
+```
+
+
+Inspect a `xid`
+```sql
+SELECT id, xid_time(id), xid_machine(id), xid_pid(id), xid_counter(id) 
+FROM (
+    SELECT xid() id FROM generate_series(1, 3)
+) a
+
+-- +--------------------+---------------------------------+-----------+-------+-----------+
+-- |id                  |xid_time                         |xid_machine|xid_pid|xid_counter|
+-- +--------------------+---------------------------------+-----------+-------+-----------+
+-- |c96r2ho8u0k0b3e6hfr0|2022-04-06 15:27:03.000000 +00:00|{8,240,40} |1421   |13011958   |
+-- |c96r2ho8u0k0b3e6hfrg|2022-04-06 15:27:03.000000 +00:00|{8,240,40} |1421   |13011959   |
+-- |c96r2ho8u0k0b3e6hfs0|2022-04-06 15:27:03.000000 +00:00|{8,240,40} |1421   |13011960   |
+-- +--------------------+---------------------------------+-----------+-------+-----------+
+```
+
+## Licenses
+The source code is licensed under the [MIT License](https://github.com/modfin/pg-xid/master/LICENSE).
---
+++ xid.sql
@@ -0,0 +1,143 @@
+
+CREATE DOMAIN public.xid AS TEXT CHECK (VALUE ~ '^[a-z0-9]{20}$');
+
+CREATE SEQUENCE xid_serial MINVALUE 1 MAXVALUE 16777215 CYCLE ;
+
+SELECT setval('xid_serial', ( random() * ((255<<16) + (255<<8) + 255))::INT);
+
+CREATE OR REPLACE FUNCTION public.xid_encode(_id int[])
+    RETURNS public.xid
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _encoding TEXT[] = '{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v}';
+BEGIN
+    RETURN _encoding[1+(_id[1] >> 3)]
+       || _encoding[1+((_id[2]>>6)&31|(_id[1]<<2)&31)]
+       || _encoding[1+((_id[2]>>1)&31)]
+       || _encoding[1+((_id[3]>>4)&31|(_id[2]<<4)&31)]
+       || _encoding[1+(_id[4]>>7|(_id[3]<<1)&31)]
+       || _encoding[1+((_id[4]>>2)&31)]
+       || _encoding[1+(_id[5]>>5|(_id[4]<<3)&31)]
+       || _encoding[1+(_id[5]&31)]
+       || _encoding[1+(_id[6]>>3)]
+       || _encoding[1+((_id[7]>>6)&31|(_id[6]<<2)&31)]
+       || _encoding[1+((_id[7]>>1)&31)]
+       || _encoding[1+((_id[8]>>4)&31|(_id[7]<<4)&31)]
+       || _encoding[1+(_id[9]>>7|(_id[8]<<1)&31)]
+       || _encoding[1+((_id[9]>>2)&31)]
+       || _encoding[1+((_id[10]>>5)|(_id[9]<<3)&31)]
+       || _encoding[1+(_id[10]&31)]
+       || _encoding[1+(_id[11]>>3)]
+       || _encoding[1+((_id[12]>>6)&31|(_id[11]<<2)&31)]
+       || _encoding[1+((_id[12]>>1)&31)]
+       || _encoding[1+((_id[12]<<4)&31)];
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.xid_decode(_xid public.xid)
+    RETURNS int[]
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _dec int[] = '{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}';
+
+BEGIN
+
+    return ARRAY[
+            ((_dec[ascii(substring(_xid,1))]<<3) | (_dec[ascii(substring(_xid, 2))]>>2)) % 256,
+            ((_dec[ascii(substring(_xid,2))]<<6) | (_dec[ascii(substring(_xid,3))]<<1) | (_dec[ascii(substring(_xid,4))]>>4)) % 256,
+            ((_dec[ascii(substring(_xid,4))]<<4) | (_dec[ascii(substring(_xid,5))]>>1)) % 256,
+            ((_dec[ascii(substring(_xid,5))]<<7) | (_dec[ascii(substring(_xid,6))]<<2) | (_dec[ascii(substring(_xid,7))]>>3)) % 256,
+            ((_dec[ascii(substring(_xid,7))]<<5) | (_dec[ascii(substring(_xid,8))])) % 256,
+            ((_dec[ascii(substring(_xid,9))]<<3) | (_dec[ascii(substring(_xid,10))]>>2)) % 256,
+            ((_dec[ascii(substring(_xid,10))]<<6) | (_dec[ascii(substring(_xid,11))]<<1) | (_dec[ascii(substring(_xid,12))]>>4)) % 256,
+            ((_dec[ascii(substring(_xid,12))]<<4) | (_dec[ascii(substring(_xid,13))]>>1)) % 256,
+            ((_dec[ascii(substring(_xid,13))]<<7) | (_dec[ascii(substring(_xid,14))]<<2) | (_dec[ascii(substring(_xid,15))]>>3)) % 256,
+            ((_dec[ascii(substring(_xid,15))]<<5) | (_dec[ascii(substring(_xid,16))])) % 256,
+            ((_dec[ascii(substring(_xid,17))]<<3) | (_dec[ascii(substring(_xid,18))]>>2)) % 256,
+            ((_dec[ascii(substring(_xid,18))]<<6) | (_dec[ascii(substring(_xid,19))]<<1) | (_dec[ascii(substring(_xid,20))]>>4)) % 256
+        ];
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION xid(_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP)
+    RETURNS public.xid
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _bytes        BYTEA;
+    _id     int[];
+BEGIN
+
+    _bytes := int8send(EXTRACT(epoch FROM _at)::BIGINT);
+    _id := ARRAY [get_byte(_bytes, 4), get_byte(_bytes, 5), get_byte(_bytes, 6) , get_byte(_bytes, 7)];
+
+    _bytes := int8send((SELECT system_identifier  FROM pg_control_system()));
+    _id := _id || ARRAY [ get_byte(_bytes, 5) , get_byte(_bytes, 6) , get_byte(_bytes, 7)];
+
+    _bytes := int4send(pg_backend_pid());
+    _id := _id || ARRAY [ get_byte(_bytes, 2), get_byte(_bytes, 3)];
+
+    _bytes := int4send(nextval('xid_serial')::INT);
+    _id := _id || ARRAY [get_byte(_bytes, 1), get_byte(_bytes, 2), get_byte(_bytes, 3)];
+
+    return public.xid_encode(_id)
+   ;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.xid_time(_xid public.xid)
+    RETURNS TIMESTAMPTZ
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _id int[];
+BEGIN
+    _id := public.xid_decode(_xid);
+    return to_timestamp( (_id[1] << 24)::BIGINT + (_id[2] << 16) + (_id[3] << 8) + (_id[4]) );
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.xid_machine(_xid public.xid)
+    RETURNS INT[]
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _id int[];
+BEGIN
+    _id := public.xid_decode(_xid);
+    return  ARRAY [_id[5], _id[6], _id[7]] ;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.xid_pid(_xid public.xid)
+    RETURNS INT
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _id int[];
+BEGIN
+    _id := public.xid_decode(_xid);
+    return  (_id[8] << 8) + (_id[9]) ;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.xid_counter(_xid public.xid)
+    RETURNS INT
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _id int[];
+BEGIN
+    _id := public.xid_decode(_xid);
+    return  (_id[10] << 16) + (_id[11] << 8) + (_id[12]) ;
+END;
+$$;
